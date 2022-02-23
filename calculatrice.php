b3f87b52152c7d214a83e22a43a88045ee1e8f0f
Youssef Kacem (Dexter) <56179828+DexterDx59@users.noreply.github.com>
2022-02-23 04:59:24 +0100

    Add files via upload

---
+++ calculatrice.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>calculatrice</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <!--<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="res/hebus_800x600_1643632014_4899.jpg" class="d-block w-1070" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="res/hebus_800x600_1643632049_3280.jpg" class="d-block w-1060" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="res/hebus_800x600_1643632071_9504.jpg" class="d-block w-1050" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <div class="container row"> 
+      <div class="col-md-4">--> 
+        <?php echo date("Y-m-d"); ?>  
+      </div> 
+    </div>
+    <?php 
+      
+      $color = "red";
+      echo "My car is " . $color . "<br>";
+      echo "My house is " . $color . "<br>";
+      echo "My boat is " . $color . "<br>";
+      
+    ?>
+      <?php 
+$result = "";
+class calculator
+{
+    var $a;
+    var $b;
+
+    function checkopration($oprator)
+    {
+        switch($oprator)
+        {
+            case '+':
+            return $this->a + $this->b;
+            break;
+
+            case '-':
+            return $this->a - $this->b;
+            break;
+
+            case '*':
+            return $this->a * $this->b;
+            break;
+
+            case '/':
+            return $this->a / $this->b;
+            break;
+
+            default:
+            return "Sorry No command found";
+        }   
+    }
+    function getresult($a, $b, $c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        return $this->checkopration($c);
+    }
+}
+
+$cal = new calculator();
+if(isset($_POST['submit']))
+{   
+    $result = $cal->getresult($_POST['n1'],$_POST['n2'],$_POST['op']);
+}
+?>
+
+<form method="post">
+<table ="center">
+    <tr>
+        <td><strong><?php echo $result; ?><strong></td>
+    </tr>
+    <tr>
+        <td>Enter 1st Number</td>
+        <td><input type="text" name="n1"></td>
+    </tr>
+
+    <tr>
+        <td>Enter 2nd Number</td>
+        <td><input type="text" name="n2"></td>
+    </tr>
+
+    <tr>
+        <td>Select Oprator</td>
+        <td><select name="op">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select></td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td><input type="submit" name="submit" value="                =                "></td>
+    </tr>
+
+</table>
+</form>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
