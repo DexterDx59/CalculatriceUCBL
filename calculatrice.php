7055cdd7ebec62aa12bef58c6335741b3c45dba8
Youssef Kacem (Dexter) <56179828+DexterDx59@users.noreply.github.com>
2022-02-23 05:12:09 +0100

    Update calculatrice.php

--- calculatrice.php
+++ calculatrice.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <!-- testing something-->
   <!--<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
